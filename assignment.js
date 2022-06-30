5501b790236398351d04e1d1554e3b47c5653871
Sameer <sameer.writes.70@gmail.com>
2022-06-30 15:48:19 +0530

    new assignment

---
+++ assignment.js
@@ -0,0 +1,14 @@
+// // Q3 -->
+
+// let n = 5;
+// let star = "";
+// let count = 1;
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         star += count;
+//         star += " ";
+//         count++;
+//     }
+//     star += "\n"
+// }
+// console.log(star);
